1d8a9593a212c3e6ec4020c13c14004620ebac7d
dagger <guptahsrh615@gmail.com>
2018-10-20 00:22:36 +0530

    Added Camera permission.

--- app/src/main/java/dining/philosophers/com/kia/PlayAreaActivity.java
+++ app/src/main/java/dining/philosophers/com/kia/PlayAreaActivity.java
@@ -2,6 +2,7 @@ package dining.philosophers.com.kia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class PlayAreaActivity extends AppCompatActivity {
 
@@ -9,5 +10,28 @@ public class PlayAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_area);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ARCore requires camera permission to operate.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
     }
 }
