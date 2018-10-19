b6be476577da051265bf83d15d6792fda09dfb12
dagger <guptahsrh615@gmail.com>
2018-10-20 01:14:28 +0530

    Added ArCore install check.

--- app/src/main/java/dining/philosophers/com/kia/MainActivity.java
+++ app/src/main/java/dining/philosophers/com/kia/MainActivity.java
@@ -1,14 +1,28 @@
 package dining.philosophers.com.kia;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private boolean mUserRequestedInstall = true;
+    private Session mSession;
     Button mStartButton;
+    private final SnackbarHelper messageSnackbarHelper = new SnackbarHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +37,73 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(playAreaIntent);
             }
         });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ARCore requires camera permission to operate.
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this);
+            return;
+        }
+
+        String message=null;
+        Exception exception=null;
+        try {
+            if (mSession == null) {
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        // Success, create the AR session.
+                        mSession = new Session(this);
+                        break;
+                    case INSTALL_REQUESTED:
+                        // Ensures next invocation of requestInstall() will either return
+                        // INSTALLED or throw an exception.
+                        mUserRequestedInstall = false;
+                        return;
+                }
+            }
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }  catch (UnavailableArcoreNotInstalledException e) {
+            message = "Please install ARCore";
+            exception = e;
+        } catch (UnavailableApkTooOldException e) {
+            message = "Please update ARCore";
+            exception = e;
+        } catch (UnavailableSdkTooOldException e) {
+            message = "Please update this app";
+            exception = e;
+        } catch (UnavailableDeviceNotCompatibleException e) {
+            message = "This device does not support AR";
+            exception = e;
+        } catch (Exception e) {
+            message = "Failed to create AR session";
+            exception = e;
+        }
+
+        if (message != null) {
+            messageSnackbarHelper.showError(this, message);
+            Log.e(TAG, "Exception creating session", exception);
+            return;
+        } // Current catch statements.
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
     }
 }
--- app/src/main/java/dining/philosophers/com/kia/PlayAreaActivity.java
+++ app/src/main/java/dining/philosophers/com/kia/PlayAreaActivity.java
@@ -11,27 +11,5 @@ public class PlayAreaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_area);
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        // ARCore requires camera permission to operate.
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this);
-            return;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                    .show();
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                CameraPermissionHelper.launchPermissionSettings(this);
-            }
-            finish();
-        }
-    }
 }
