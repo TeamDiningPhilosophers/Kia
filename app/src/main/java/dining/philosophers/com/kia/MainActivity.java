7d00c887488f0f14d1e311242e1a6907a9bf7224
noble-gas <bansalakshit56@gmail.com>
2018-10-20 00:03:23 +0530

    Added basic dependencies

--- app/src/main/java/dining/philosophers/com/kia/MainActivity.java
+++ app/src/main/java/dining/philosophers/com/kia/MainActivity.java
@@ -1,13 +1,27 @@
 package dining.philosophers.com.kia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = findViewById(R.id.startGameButton);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playAreaIntent = new Intent(MainActivity.this, PlayAreaActivity.class);
+                startActivity(playAreaIntent);
+            }
+        });
     }
 }
